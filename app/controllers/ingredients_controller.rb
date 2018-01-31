f18ac6b7718a29a9ac083be9b0e8f6db25f8d2de
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 22:45:36 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -22,7 +22,7 @@ class IngredientsController < ApplicationController
   def show
     @ingredient = Ingredient.find(params[:id])
   end
-  
+
   private
   def ingredient_params
     params.require(:ingredient).permit(:ingredient_name)
