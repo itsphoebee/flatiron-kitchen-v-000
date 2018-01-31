baf02a312b5c3ef8654d3e4e15049772ced284df
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 22:31:25 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -6,5 +6,10 @@ class IngredientsController < ApplicationController
   def create
     @ingredient = Ingredient.new(ingredient_params)
   end
-  
+
+  private
+  def ingredient_params
+    params.require[:ingredient].permit[:name]
+  end
+
 end
