db1474c346df793f103b0f1d4f7cf3f41a2694aa
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 22:29:14 +0000

    automatically backed up by learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,4 +2,12 @@ class Ingredient < ActiveRecord::Base
   validates :name, presence: true
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
+
+  def ingredient_name=(name)
+
+  end
+
+  def ingredient_name
+    self.name
+  end
 end
