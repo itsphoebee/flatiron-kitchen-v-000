c02d74447a94c3ac412465b494f81771e3f39e66
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 23:24:10 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -41,6 +41,6 @@ class RecipesController < ApplicationController
   end
 
   def recipe_params
-    params.require(:recipe).permit(:name, :ingredient_ids=>[], )
+    params.require(:recipe).permit(:name, :ingredient_ids=>[], :ingredients_attributes{})
   end
 end
