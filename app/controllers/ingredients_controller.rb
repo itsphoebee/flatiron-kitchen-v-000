50059d584dbf991ba85cfd70f077bd2ee72388f6
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 22:30:57 +0000

    automatically backed up by learn

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -2,4 +2,9 @@ class IngredientsController < ApplicationController
   def new
     @ingredient = Ingredient.new
   end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+  end
+  
 end
