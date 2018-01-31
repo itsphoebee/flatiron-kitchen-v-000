7502a145255636ab18edf805b5b92ab80e158fc8
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 23:01:34 +0000

    automatically backed up by learn

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,11 +4,4 @@ class Recipe < ActiveRecord::Base
   has_many :ingredients, through: :recipe_ingredients
   accepts_nested_attributes_for :ingredients
 
-  def ingredient_name=(name)
-    self.name = name
-  end
-
-  def ingredient_name
-    self.name
-  end
 end
