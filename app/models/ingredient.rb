c8f68913a9877f31aae6b3b56d984818a5f2c6c7
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 23:06:19 +0000

    automatically backed up by learn

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -3,11 +3,4 @@ class Ingredient < ActiveRecord::Base
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
-  def ingredient_name=(name)
-    self.name = name
-  end
-
-  def ingredient_name
-    self.name
-  end
 end
