34431002e54bb79a545efe193221268dd9b75130
Phoebe Lau <phoebelau7@gmail.com>
2018-01-31 23:34:59 +0000

    automatically backed up by learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
-    #@recipe.ingredients.build(name:'ingredient_name')
+    @recipe.ingredients.build(name:' ')
   end
 
   def create
